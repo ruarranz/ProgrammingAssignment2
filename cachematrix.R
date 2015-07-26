33e39d056486d555b23234347c28f580eeaabe33
ruarranz <ruarranz@gmail.com>
2015-07-26 17:24:44 +0200

    Programming Assignment 2
    Assignment: Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse. 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        ## Commit test
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
 }
+
+
+#####################################################################
+##                                                                 ##                                                      
+##                       Testing functions                         ##
+##                                                                 ##
+#####################################################################
+
+M <- matrix( c(10,2,3,4,5,6,7,8,9),  nrow=3, ncol=3) 
+specialMatrix <- makeCacheMatrix(M)
+INV <- cacheSolve(specialMatrix)
+## Now we have the data in cache
+INV <- cacheSolve(specialMatrix)
+INV <- cacheSolve(specialMatrix)
+INV <- cacheSolve(specialMatrix)
+
+## If the data is correct, this operation must return 3x3 identity matrix
+M %*% INV
+INV %*% M
